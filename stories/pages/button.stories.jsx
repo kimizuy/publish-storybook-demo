eacbad0469561deac41f09129b00a241bb0c02b7
kimizuy <kimizuyamasaki@gmail.com>
2022-05-19 16:17:22 +0900

    add button story

---
+++ stories/pages/button.stories.jsx
@@ -0,0 +1,20 @@
+export default {
+  title: 'Component',
+}
+
+export const Button = () => (
+  <button
+    style={{
+      height: '58px',
+      width: '130px',
+      border: 'none',
+      borderRadius: '5px',
+      padding: '15px 40px',
+      color: 'white',
+      backgroundColor: '#23A6F0',
+      fontWeight: 'bold',
+    }}
+  >
+    Button
+  </button>
+)
